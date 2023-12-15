05f459296e7ba0af66b79c5e6f8431b8063f658e
252861 <105175211+252861@users.noreply.github.com>
2023-12-15 16:37:44 +0000

    ok

--- DataRequestor/main.py
+++ DataRequestor/main.py
@@ -7,30 +7,27 @@ import requests
 url = "https://api.openaq.org/v2/latest/9342?limit=100&page=1&offset=0&sort=asc"
 
 headers = {"accept": "application/json"}
-#test
 
 while (True):
     response = requests.get(url, headers=headers)
     x = response.text
     y = json.loads(x)
-                       
-     
-    weatherRequest={"location":"<location-name>",
-                    "timestamp":"<iso-current-time>",
-                    "values":[{"<measurement-name>":"<value>"}]}
     
-    dKraj = y['results'][0]['country']
-    dMiasto = y['results'][0]['city']
-    dCzas = str(y['results'][0]['measurements'][0]['lastUpdated'])
-    dCzasF = dCzas.replace("T"," ")
-    
-    #print("Kraj: "      +  y['results'][0]['country'])
-    #print("Miasto: "    +  y['results'][0]['city'])
-    #print("Czas: "      +  str(y['results'][0]['measurements'][0]['lastUpdated']))
+    kraj = y['results'][0]['country']
+    miasto = y['results'][0]['city']
+    czas = str(y['results'][0]['measurements'][0]['lastUpdated'])
 
-    print("Kraj: "      +  dKraj)
-    print("Miasto: "    +  dMiasto)
-    print("Czas: "      +  dCzasF)
+    location={"country":kraj,
+             "city":miasto}
+    
+    values={}
+
+    weather={   "location":location,
+                "timestamp":czas,
+                "values":[{<measurement-name>:<value>}]}
+
+    data_to_send=json.dumps(weather)
+    print(data_to_send)
 
     for el in y["results"][0]["measurements"]:
         print(f"{el['parameter']:<5} = {el['value']} {el['unit']}")
