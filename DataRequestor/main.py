a89a1c15183de21196abf12b891330fcbca01bc7
252861 <105175211+252861@users.noreply.github.com>
2024-01-12 17:02:46 +0000

    gites, zajebiscie, dziiala, mozna przejsc do mqtt

--- DataRequestor/main.py
+++ DataRequestor/main.py
@@ -5,27 +5,27 @@ import json
 import os
 import requests
 
-# SAM WROCŁAW ----------------------
-# url = "https://api.openaq.org/v2/latest/9342?limit=100&page=1&offset=0&sort=asc"
-# headers = {"accept": "application/json"}
 
+env_city = os.getenv('CITY')
 
-url = "https://api.openaq.org/v2/locations?limit=100&page=1&offset=0&sort=desc&radius=1000&country=PL&order_by=lastUpdated&dump_raw=false"
+#url = "https://api.openaq.org/v2/locations?limit=100&page=1&offset=0&sort=desc&radius=1000&country=PL&city=E%C5%82k&order_by=lastUpdated&dump_raw=false"
+url = f"https://api.openaq.org/v2/locations?limit=100&page=1&offset=0&sort=desc&radius=1000&country=PL&city={env_city}&order_by=lastUpdated&dump_raw=false"
 headers = {"accept": "application/json"}
 
 
 while (True):
+    
     response = requests.get(url, headers=headers)
     x = response.text
     y = json.loads(x)
-    
+  
 
     country = y['results'][0]['country']
     city = y['results'][0]['city']
     time = str(y['results'][0]['parameters'][0]['lastUpdated'])
 
     location={"country":country,
-             "city":city}
+            "city":city}
     
     values={}
     for el in y["results"][0]["parameters"]:
@@ -42,6 +42,7 @@ while (True):
     print(data_to_send)
 
     sleep(5)
+
 
 
 
