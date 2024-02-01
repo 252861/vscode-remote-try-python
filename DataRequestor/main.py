dce2b2359a3f12ec8b5de5aad271456c63991813
252861 <105175211+252861@users.noreply.github.com>
2024-02-01 20:28:16 +0000

    k

--- DataRequestor/main.py
+++ DataRequestor/main.py
@@ -1,92 +1,37 @@
 from time import sleep
 from datetime import datetime
 from paho.mqtt import client as mqtt_client
-#from snap7.server import mainloop
 import json
 import os
 import requests
 import time
-import mqttpub
+from mqttps import *
 
+url = f"https://api.openaq.org/v2/locations?limit=100&page=1&offset=0&sort=desc&radius=1000&country=PL&city={os.getenv('CITY')}&order_by=lastUpdated&dump_raw=false"
+values={}
 
-
-env_city = os.getenv('CITY')
-broker = os.getenv('IP')
-port = 1883
-topic = f"252861/{os.getenv('CITY')}"
-client_id = "252861"
-username = os.getenv('USER')
-password = os.getenv('PASS')
-
-url = f"https://api.openaq.org/v2/locations?limit=100&page=1&offset=0&sort=desc&radius=1000&country=PL&city={env_city}&order_by=lastUpdated&dump_raw=false"
-headers = {"accept": "application/json"}
-
-class MqttPub:
-
-    def connect_mqtt(self):
-        def on_connect(client, userdata, flags, rc):
-            if rc == 0:
-                print("Connected to MQTT Broker!")
-            else:
-                print(f"Failed to connect, return code {rc}\n")
-
-        self.client = mqtt_client.Client(client_id)
-        self.client.username_pw_set(username, password)
-        self.client.on_connect = on_connect
-        self.client.connect(broker, port)
-        return self.client
-
-
-    def publish(self,data_to_send):
-
-        result = self.client.publish(topic, data_to_send)
-        # result: [0, 1]
-        status = result[0]
-        if status == 0:
-            print(f"Send `{data_to_send}` to topic `{topic}`")
-        else:
-            print(f"Failed to send message to topic {topic}")
-
-     
-
-mymmqttpublish = MqttPub()
-mymmqttpublish.connect_mqtt()
+mqttps = mqttps(str(os.getenv('IP')), str("252861/"+str(os.getenv('CITY'))), str(os.getenv('USER')), str(os.getenv('PASS')))
+mqttps.connect_mqtt()
       
-
 while (True):
     
-    response = requests.get(url, headers=headers)
+    response = requests.get(url)
     x = response.text
     y = json.loads(x)
   
-
     country = y['results'][0]['country']
     city = y['results'][0]['city']
     time = str(y['results'][0]['parameters'][0]['lastUpdated'])
 
-    location={"country":country,
-            "city":city}
-    
-    values={}
-    # for el in y["results"][0]["parameters"]:
-    #     values[el["parameter"]]={
-    #         "value":el["lastValue"],
-    #         "unit":el["unit"]
-    #     }
+    location={"country":country, "city":city}
 
     for el in y["results"][0]["parameters"]:
         values[el["parameter"]]=el["lastValue"]
 
-    weather={   "location":location["city"],
-                "timestamp":time,
-                "values":values}
+    weather={   "location":location["city"], "timestamp":time, "values":values}
 
     data_to_send=json.dumps(weather,ensure_ascii=False)
     print(data_to_send)
 
-    mymmqttpublish.publish(data_to_send)
+    mqttps.publish(data_to_send)
     sleep(5)
-
-
-# do poprawy czas w "weather": ostanie laby 26.01.2024 reszta względnie git
-
---
+++ DataRequestor/mqttsp.py
@@ -0,0 +1,50 @@
+
+import time
+import os
+from paho.mqtt import client as mqtt_client
+
+class mqttps:
+    def __init__(self, ip, city, user, passwd):
+        self.ip = ip
+        self.city = city
+        self.user = user
+        self.passwd = passwd
+        self.client_id = "252861"
+        self.port = 1883
+        self.topic = self.client_id + "/" + self.city
+
+    def connect_mqtt(self):
+        def on_connect(client, userdata, flags, rc):
+            if rc == 0:
+                self.subscribe()
+                print("Connected to MQTT Broker!")
+                
+            else:
+                print(f"Failed to connect, return code {rc}\n")
+
+        self.client = mqtt_client.Client(self.client_id)
+        self.client.username_pw_set(self.user, self.passwd)
+        self.client.loop_start()
+        self.client.on_connect = on_connect
+        self.client.connect(self.ip, self.port)
+        return self.client
+
+    def publish(self,data_to_send):
+
+        result = self.client.publish(self.topic, data_to_send)
+        status = result[0]
+        if status == 0:
+            print(f"Send `{data_to_send}` to topic `{self.topic}`")
+        else:
+            print(f"Failed to send message to topic {self.topic}")
+    
+    def subscribe(self):
+        def on_message(client, userdata, msg):
+            print(msg.payload.decode())
+            name = str(msg.topic)
+            name = name.replace("/","-")
+            path = os.path.join("Data", name)                
+            with open(path, 'w') as file: 
+                file.write(msg.payload.decode())  
+        self.client.subscribe(self.topic)
+        self.client.on_message = on_message
