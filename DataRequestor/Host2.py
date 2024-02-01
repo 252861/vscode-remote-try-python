08627bbe0131d72fba34f38c73da9caefb1fef20
252861 <105175211+252861@users.noreply.github.com>
2024-02-01 21:12:35 +0000

    k

---
+++ DataRequestor/Host2.py
@@ -0,0 +1,16 @@
+import os,glob
+from flask import *
+import json
+from jinja2 import Template
+
+app = Flask(name)
+@app.route("/") 
+
+def hello_world(): 
+    data = []
+    for filename in glob.glob(os.path.join("Data/", "*")):
+        with open(filename) as json_file:
+                json_data = json.load(json_file)
+        data.append(json_data)
+
+    return render_template('index1.html', data_list=data)
